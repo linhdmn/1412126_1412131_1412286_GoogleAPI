49de64ece01a4b8fa697260ec7780acef940d906
Vo Thanh Dien <vtdien96@gmail.com>
2017-10-12 21:05:43 +0700

    them su kien click

--- javascript/myJavascript.js
+++ javascript/myJavascript.js
@@ -2,6 +2,7 @@ var map;
 var isDirecting = false;
 var isCurrent = false;
 var curOri;
+
 //hover show text
 $("#button_search").hover(function() {
   $("#search-text").show();
@@ -29,12 +30,15 @@ $(document).ready(function(){
   map = new GMaps({
     el: '#map',
     lat: 10.7626391,
-    lng: 106.6820268
+    lng: 106.6820268,
+    click: function(event) {
+      clickOnMap(event);
+    }
   });
   map.setContextMenu({
     control: 'map',
     options: [{
-      title: 'Địa điểm của bạn',
+      title: 'Add maker here',
       name: 'add_marker',
       action: function(e){
         console.log(e.latLng.lat());
@@ -42,7 +46,7 @@ $(document).ready(function(){
         this.addMarker({
           lat: e.latLng.lat(),
           lng: e.latLng.lng(),
-          title: 'Địa điểm của bạn'
+          title: 'Add maker here'
         });
         this.hideContextMenu();
       }
@@ -54,20 +58,12 @@ $(document).ready(function(){
       }
     }]
   });
-  map.setContextMenu({
-    control: 'marker',
-    options: [{
-      title: 'Center here',
-      name: 'center_here',
-      action: function(e){
-        this.setCenter(e.latLng.lat(), e.latLng.lng());
-      }
-    }]
-  });
 });
 //=======================================
-// search
-$("#button_search").click(function(event) {
+$(".btn").click(function(event) {
+  //
+  //nhan button search
+  if(this.id=="button_search"){
     var str = $("#inputAddress").val();
     if(str !== "" && isDirecting === false){
       findAddress(str);
@@ -77,10 +73,50 @@ $("#button_search").click(function(event) {
       var des = $("#inputDestinations").val();
       directing(str,des);
     }
+  }
+// nhan show location
+  if(this.id == "button_location"){
+    GMaps.geolocate({
+      success: function(position){
+        map.setCenter(position.coords.latitude, position.coords.longitude);
+        map.addMarker({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          });
+        curOri = position;
+        isCurrent = true;
+        $("#inputAddress").val(position.coords.latitude, position.coords.longitude);
+      },
+      error: function(error){
+        alert('Geolocation failed: '+error.message);
+      },
+      not_supported: function(){
+        alert("Your browser does not support geolocation");
+      },
+    });
+  }
+//-------------------------------
+//Nhan direction sẽ hiện và ẩn input destination đồng thời bật tắt chế độ chỉ đường
+  if(this.id == "button_direct"){
+    if($(".hidden").is(":hidden")){
+      $(".hidden").show();
+      $("#inputAddress").attr({
+        placeholder: 'Enter starting..',
+      });
+      isDirecting = true;
+    }else{
+      $(".hidden").hide();
+      $("#inputAddress").attr({
+        placeholder: "Search...",
+      });
+      isDirecting = false;
+    }
+  }
+  //
 });
 $("#inputAddress").keyup(function(event) {
   var str = $("#inputAddress").val();
-  if(event.keyCode == 13 && str !== "")
+  if(event.keyCode == 13 && str !== "" && isDirecting === false)
   {
     findAddress(str);
   }
@@ -100,28 +136,13 @@ function findAddress(str){
     }
   });
 };
-//=========================================
-//geolocation
-$("#button_location").click(function(event) {
-  GMaps.geolocate({
-    success: function(position){
-      map.setCenter(position.coords.latitude, position.coords.longitude);
-      map.addMarker({
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        });
-      curOri = position;
-      isCurrent = true;
-      $("#inputAddress").val(position.coords.latitude, position.coords.longitude);
-    },
-    error: function(error){
-      alert('Geolocation failed: '+error.message);
-    },
-    not_supported: function(){
-      alert("Your browser does not support geolocation");
-    },
-  });
-});
+//================================
+function clickOnMap(event){
+  console.log(event.placeId);
+}
+
+
+
 //============================================
 function directing(ori,des){
   if(isCurrent == true){
@@ -177,20 +198,3 @@ function directing(ori,des){
   }
   
 }
-//-------------------------------
-//Nhan direction sẽ hiện và ẩn input destination đồng thời bật tắt chế độ chỉ đường
-$("#button_direct").click(function(event) {
-  if($(".hidden").is(":hidden")){
-    $(".hidden").show();
-    $("#inputAddress").attr({
-      placeholder: 'Enter starting..',
-    });
-    isDirecting = true;
-  }else{
-    $(".hidden").hide();
-    $("#inputAddress").attr({
-      placeholder: "Search...",
-    });
-    isDirecting = false;
-  }
-});
