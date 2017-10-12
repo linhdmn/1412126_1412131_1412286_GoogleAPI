4e67940a6540c4e3edd83b4097d6a9f094a2de0a
Vo Thanh Dien <vtdien96@gmail.com>
2017-10-12 23:05:41 +0700

    them chức năng swap,

--- javascript/myJavascript.js
+++ javascript/myJavascript.js
@@ -113,6 +113,13 @@ $(".btn").click(function(event) {
       isDirecting = false;
     }
   }
+// nhan swap doi value cua des và origin
+  if(this.id == "button_swap"){
+    var str = $("#inputAddress").val();
+    var des = $("#inputDestinations").val();
+    $("#inputAddress").val(des);
+    $("#inputDestinations").val(str);
+  }
   //
 });
 $("#inputAddress").keyup(function(event) {
@@ -140,9 +147,42 @@ function findAddress(str){
 //================================
 function clickOnMap(event){
   console.log(event.placeId);
+  // if(event.placeId){
+  //   $.ajax({
+  //     url: 'https://maps.googleapis.com/maps/api/place/details/json?placeid=' + event.placeId + 
+  //     '&key=AIzaSyCqG55x6-7BVevi2doMzzPFqmmATL55iPU',
+  //     type: 'GET',
+  //     contentType:'text/plain',
+  //     xhrFields: {
+  //     // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
+  //     // This can be used to set the 'withCredentials' property.
+  //     // Set the value to 'true' if you'd like to pass cookies to the server.
+  //     // If this is enabled, your server must respond with the header
+  //     // 'Access-Control-Allow-Credentials: true'.
+  //     withCredentials: false
+  //     },
+
+  //     headers: {
+  //       // Set any custom headers here.
+  //       // If you set any non-simple headers, your server must include these
+  //       // headers in the 'Access-Control-Allow-Headers' response header.
+  //     },
+
+  //     success: function() {
+  //       console.log("success");
+  //       // Here's where you handle a successful response.
+  //     },
+
+  //     error: function() {
+  //       console.log("error");
+  //       // Here's where you handle an error response.
+  //       // Note that if the error was due to a CORS issue,
+  //       // this function will still fire, but there won't be any additional
+  //       // information about the error.
+  //     }
+  //   });  
+  // }
 }
-
-
 
 //============================================
 function directing(ori,des){
